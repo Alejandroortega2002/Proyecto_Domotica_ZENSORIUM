01a471cb99876931ceb8032b11044f273c2f5d9c
Alejandro Ortega Núñez <99034782+Alejandroortega2002@users.noreply.github.com>
2024-01-10 17:57:52 +0100

    mejoras

---
+++ src/entidades/NodoUsuarios.java
@@ -0,0 +1,17 @@
+package entidades;
+
+public class NodoUsuarios {
+	
+	private Usuario usuario;
+	private NodoUsuarios siguiente;
+	
+	public NodoUsuarios(Usuario usuario) {
+		super();
+		this.usuario = usuario;
+		this.siguiente = null;
+	}
+	
+	
+
+	
+}
